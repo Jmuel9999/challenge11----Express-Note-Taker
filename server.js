59d3b705da2494196a47c146dd99a3a4e8c36180
jasonmueller <jmuel9999@gmail.com>
2021-05-19 16:44:18 -0500

    Removed useless files, added get routes for index.html and notes.html

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const express = require('express');
 // instantiate (define) the server
 const app = express();
 
+// brings up note taker homepage
+app.get('/*', function(req, res) {
+    res.sendFile('./public/index.html', {root: __dirname})
+});
 
-
+// link notes.html to the index.html "get started" button
+app.get('/notes', function(req, res) {
+    res.sendFile('/public/notes.html', {root: __dirname});
+});
 
 // tells server to listen for requests
-app.listen(3001, () => {
+app.listen(3000, () => {
     console.log(`API server now on port 3000!`);
 });
