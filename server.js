1c15ae47b6ec096c9fb0fe5ebba4957b3f085e08
jasonmueller <jmuel9999@gmail.com>
2021-05-19 17:52:21 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -2,10 +2,22 @@
 const express = require('express');
 // instantiate (define) the server
 const app = express();
+const db = require('./db/db.json');
 
 // link notes.html to the index.html "get started" button
 app.get('/notes', function(req, res) {
     res.sendFile('/public/notes.html', {root: __dirname});
+});
+
+// get data from db.json and return all notes
+app.get('/api/notes', function(req, res) {
+    res.json(db);
+});
+
+// add to notes.html and db.json, return to client
+app.post('/api/notes', (req, res) => {
+    
+    //req.body.id = db.length.toString();
 });
 
 // brings up index.html
@@ -13,13 +25,7 @@ app.get('/*', function(req, res) {
     res.sendFile('/public/index.html', {root: __dirname})
 });
 
-// get data from db.json and return all notes
-app.get('/api/notes', function(req, res) {
-    res.json('/db/db.json');
-});
 
-// add to notes.html and db.json, return to client
-app.post('/api/notes')
 
 // tells server to listen for requests
 app.listen(3000, () => {
